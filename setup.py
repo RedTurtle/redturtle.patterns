3e804a423292f438fa6991f7e8db81728b0e2dd6
Piero Nicolli <piero.nicolli@redturtle.it>
2019-06-06 10:32:08 +0200

    Removed temporary require call

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='redturtle.patterns',
-    version='1.1unabledpatterns',
+    version='1.1.1unabledpatterns',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -22,6 +22,7 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Operating System :: OS Independent",
