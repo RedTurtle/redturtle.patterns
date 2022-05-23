93e94be810ece10b587872b08aa8f89514fb4c5d
Riccardo Lemmi <riccardo@reflab.com>
2022-05-23 12:03:22 +0200

    major refactoring for python 3.8 and plone 5.2

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
@@ -38,10 +38,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'plone.api',
         'Products.GenericSetup>=1.8.2',
         'setuptools',
-        'z3c.jbot',
     ],
     extras_require={
         'test': [
